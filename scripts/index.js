11a85d93058f716c663d39510ce8d242d7853094
Casey88 <cmarie.rum@gmail.com>
2021-09-26 16:07:23 -0600

    update js foundation

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,10 @@
-const
+const openFormButton = document.querySelector('.profile__edit-button')
+const popup = document.querySelector('.modal');
+const closeButton = popup.querySelector('.form__close-button');
+
+function toggleForm() {
+  popup.classList.toggle('modal_visible');
+}
+
+openFormButton.addEventListener('click', toggleForm);
+closeButton.addEventListener('click', toggleForm);
